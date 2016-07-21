b87eb40e5574924ba0b5cbc87ecd2daa161e4086
bariteau <bariteau@yelp.com>
2016-07-21 16:27:31 -0700

    add preliminary ability to render text

--- cmd/atwgen/atwgen.go
+++ cmd/atwgen/atwgen.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/draw"
 	_ "image/jpeg"
 	"image/png"
+	"io/ioutil"
 	"os"
-	// "github.com/golang/freetype"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
 func mustGetImage(path string) image.Image {
@@ -64,6 +69,66 @@ func writeBackground(destinationImage draw.Image) draw.Image {
 	return destinationImage
 }
 
+func getFont() *truetype.Font {
+	fontFd, err := os.Open("Loveletter_TW.ttf")
+	if err != nil {
+		panic(err)
+	}
+
+	fontBytes, err := ioutil.ReadAll(fontFd)
+	if err != nil {
+		panic(err)
+	}
+
+	font, err := truetype.Parse(fontBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return font
+}
+
+func writeText(textConfig []string, destinationImage draw.Image) draw.Image {
+	if len(textConfig) > 3 {
+		panic(errors.New("more than three captions specified"))
+	}
+
+	fontFace := truetype.NewFace(
+		getFont(),
+		&truetype.Options{Size: 16.0},
+	)
+	x0 := 30
+	y1 := 120
+	startPoint := fixed.P(x0, y1)
+	drawer := &font.Drawer{
+		Dst:  destinationImage,
+		Src:  image.Black,
+		Face: fontFace,
+		Dot:  startPoint,
+	}
+
+	drawDistance := drawer.MeasureString(textConfig[0])
+	paddingPixels := 2
+	borderRect := image.Rect(
+		x0-paddingPixels,
+		y1-fontFace.Metrics().Ascent.Round()-paddingPixels,
+		x0+drawDistance.Round()+paddingPixels,
+		y1+paddingPixels,
+	)
+	draw.DrawMask(
+		destinationImage,
+		destinationImage.Bounds(),
+		image.White,
+		image.ZP,
+		borderRect,
+		image.ZP,
+		draw.Over,
+	)
+	drawer.DrawString(textConfig[0])
+
+	return destinationImage
+}
+
 func writeImage(path string, image image.Image) error {
 	fd, err := os.Create(path)
 	if err != nil {
@@ -75,10 +140,11 @@ func writeImage(path string, image image.Image) error {
 }
 
 func main() {
-	destinationImage := writeBackground(generateBasicTemplate())
+	destinationImage := writeText([]string{"foo bar"}, writeBackground(generateBasicTemplate()))
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println(r)
+			os.Exit(1)
 		}
 	}()
 
