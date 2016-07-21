4256dd768ad47d81823b465166ed920cfdebb6e8
bariteau <bariteau@yelp.com>
2016-07-21 12:38:42 -0700

    basic (crappy) stuff for just background

---
+++ cmd/atwgen/atwgen.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"image"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
+	"os"
+)
+
+func getImage(path string) image.Image {
+	imageFd, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer imageFd.Close()
+
+	image, _, err := image.Decode(imageFd)
+	if err != nil {
+		panic(err)
+	}
+	return image
+}
+
+func main() {
+	templateImage := getImage("template.png")
+	backgroundImage := getImage("background")
+
+	destinationImage := image.NewNRGBA(templateImage.Bounds())
+
+	// put base template into our destination
+	draw.Draw(
+		destinationImage,
+		destinationImage.Bounds(),
+		templateImage,
+		image.ZP,
+		draw.Src,
+	)
+
+	draw.DrawMask(
+		destinationImage,
+		destinationImage.Bounds(),
+		backgroundImage,
+		image.ZP,
+		templateImage,
+		image.ZP,
+		draw.Over,
+	)
+
+	fd, err := os.Create("out.png")
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+
+	err = png.Encode(fd, destinationImage)
+	if err != nil {
+		panic(err)
+	}
+}
